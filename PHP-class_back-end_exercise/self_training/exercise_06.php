1f7850ac95008bc3cb829c56259feae8450e06e0
ChristopheLin <fengxup@gmail>
2025-11-17 00:35:25 +0800

    練習6-新增圖形：正三角形和等腰三角形

--- PHP-class_back-end_exercise/self_training/exercise_06.php
+++ PHP-class_back-end_exercise/self_training/exercise_06.php
@@ -66,7 +66,61 @@
     </div>
     <fieldset>
         <legend>圖形-正三角形-01</legend>
-        
+        <?php
+        for($i=0;$i<5;$i++){
+            for($k=0;$k<=(4-$i);$k++){
+                echo "&nbsp;";
+            }
+            for($j=0;$j<(2*$i+1);$j++){
+                echo "*";
+            }
+            echo "<br>";
+        }
+            ?>
+    </fieldset>
+    <div>
+
+    </div>
+    <fieldset>
+        <legend>圖形-等腰三角形-02</legend>
+        <?php
+        for($i=0;$i<=9;$i++){
+            if($i<=4){
+                for($k=0;$k<(4-$i);$k++){
+                    echo "&nbsp;";
+                }
+                for($j=0;$j<=$i;$j++){
+                    echo "*";
+                }
+                    
+            }else{
+                for($k=0;$k<($i-4);$k++){
+                    echo "&nbsp;";
+                }
+                for($j=0;$j<(9-$i);$j++){
+                    echo "*";
+                }
+            }
+            echo "<br>";
+        }
+        ?>
+    </fieldset>
+    <div>
+
+    </div>
+    <fieldset>
+        <legend>圖形-正三角形-02</legend>
+        <?php
+        for($i=0;$i<5;$i++){
+            for($k=0;$k<=$i;$k++){
+                echo "&nbsp;";
+            }
+            for($j=0;$j<=(2*(4-$i));$j++){
+                echo "*";
+            }
+            echo "<br>";
+        }
+        ?>
     </fieldset>
 </body>
 </html>
