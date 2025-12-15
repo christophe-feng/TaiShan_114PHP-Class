60f494eb5a39014e65a04dff9975f8bfed58d960
christophe-feng <fengxup@gmail.com>
2025-12-15 16:34:49 +0800

    新增今日上課內容：jquery-each,fade,slide

---
+++ PHP-class_front-end_exercise/s20251215/data.php
@@ -0,0 +1,29 @@
+<?php
+
+$myArr = [1, 2, 3, 4, 5];
+
+// fetch
+$assocArr = [
+    "name" => "John",
+    "age" => 30,
+    "city" => "New York"
+];
+
+// fetchAll
+$data = [
+    [
+        'id' => 1,
+        'name' => 'amy',
+        'mobile' => '0911'
+    ],
+    [
+        'id' => 2,
+        'name' => 'bob',
+        'mobile' => '0922'
+    ],
+    [
+        'id' => 3,
+        'name' => 'cat',
+        'mobile' => '0933'
+    ],
+];
